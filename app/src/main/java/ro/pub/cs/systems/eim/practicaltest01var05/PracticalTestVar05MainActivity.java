da2898f16d8510efdaaf1ccd28e5eafe77cebdef
Bogdan Dobrin <b.dobrin@yahoo.com>
2022-04-11 08:18:32 +0300

    A1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTestVar05MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest01var05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTestVar05MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activituy_practical_test01_var05_main);
+    }
+}
